0f21422e94006a4abb7e32372253be40cff5231f
Oliver Emmanuel C. Quiroz <oliver.emmanuel.quiroz@gmail.com>
2024-03-06 13:28:01 -0800

    add sql connection

--- config.php
+++ config.php
@@ -10,3 +10,42 @@ $conn = new mysqli($servername, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+// Handle add task
+if ($_POST['action'] == 'addTask') {
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $date = $_POST['date'];
+
+    $sql = "INSERT INTO tasks (title, description, date) VALUES ('$title', '$description', '$date')";
+    $conn->query($sql);
+
+    // Get the inserted task ID
+    $taskId = $conn->insert_id;
+
+    $response = array('taskId' => $taskId);
+    echo json_encode($response);
+}
+
+// Handle delete task
+if ($_POST['action'] == 'deleteTask') {
+    $taskId = $_POST['taskId'];
+
+    $sql = "DELETE FROM tasks WHERE id = $taskId";
+    $conn->query($sql);
+}
+
+// Handle get tasks
+if ($_POST['action'] == 'getTasks') {
+    $tasks = array();
+
+    $result = $conn->query("SELECT * FROM tasks");
+    while ($row = $result->fetch_assoc()) {
+        $tasks[] = $row;
+    }
+
+    echo json_encode($tasks);
+}
+
+$conn->close();
+?>
